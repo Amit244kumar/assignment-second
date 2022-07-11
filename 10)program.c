701f9c4cfa0fa98a7e76ade9d777cd55587c60fa
Amit244kumar <108994665+Amit244kumar@users.noreply.github.com>
2022-07-11 19:35:37 +0530

    Delete 10)program.c

--- 10)program.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-     int date,month,year;
-     printf("Enter date , month , year\n");
-     scanf("%d/%d/%d",&date,&month,&year);
-     printf("Date-%d ,Month-%d ,Year-%d",date,month,year);
-     getch();
-     return 0;
-}
