7bf612ce7e473323d5b83d569929b41a2ebba404
Amit244kumar <108994665+Amit244kumar@users.noreply.github.com>
2022-07-11 19:35:46 +0530

    Delete 11)program.c

--- 11)program.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    int hours,minutes;
-    printf("Enter hours and minutes\n");
-    scanf("%d:%d",&hours,&minutes);
-    printf("%d hours and %d minutes",hours,minutes);
-    getch();
-    return 0;
-}
